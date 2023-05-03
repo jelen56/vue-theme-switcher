98183a6b7bf08f4552ad1816d1d7e3e529334932
jelen <jelen56@163.com>
2023-05-03 09:37:45 +0800

    change the Demo.vue name

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template v-bind:="needReload" class="main" >
   <img alt="Vue logo" src="./assets/logo.png">
-  <Demo msg="Welcome to Your Vue.js App"/>
+  <SimpleDemo msg="Welcome to Your Vue.js App"/>
 </template>
 
 <script setup>
 import {nextTick, provide, ref} from "vue";
-import Demo from "@/components/Demo";
+import SimpleDemo from "@/components/SimpleDemo";
 const needReload=ref(false);
 async function reload(){
   needReload.value=true;
